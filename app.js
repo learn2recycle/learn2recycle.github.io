95bc148a516ee546b5fa90e97040cfe3d9228d10
learn2recycle <joechysci@hotmail.com>
2016-01-22 21:56:59 +0000

    jstestexternal

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // JavaScript File
  document.getElementById('game_button').onclick = function() {
-   var iframe = document.createElement('iframe');
-   iframe.src = 'google.com';
-   document.body.appendChild(iframe);
+  // var iframe = document.createElement('iframe');
+   //iframe.src = 'google.com';
+   //document.body.appendChild(iframe);
+   window.alert('hi person!!!')
 
 };
