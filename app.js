c362a03faaab7f3e2ca81a8c3073f82fce73a50b
learn2recycle <joechysci@hotmail.com>
2016-01-23 01:03:16 +0000

    yes,iframetest2

--- app.js
+++
@@ -1,8 +0,0 @@
-// JavaScript File
- document.getElementById('game_button').onclick = function() {
-  // var iframe = document.createElement('iframe');
-   //iframe.src = 'google.com';
-   //document.body.appendChild(iframe);
-   window.alert('hiperson');
-
-};
